2db5fc13f2988436e78316bbfdb45070d6f89e4f
DESKTOP-2BJM56G\Hyeseon <artsmenii@gmail.com>
2021-08-13 16:22:19 +0900

    mydict first commit

--- main.go
+++ main.go
@@ -2,16 +2,11 @@ package main
 
 import (
 	"fmt"
-	"go_test/modules/accounts"
+	"go_test/modules/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("sun")
-	account.Deposit(10)
-	err := account.Withdraw(20) //balance가 -가 되지 않도록 예외처리를 해야한다.
-	if err != nil {
-		fmt.Println(err)
-		//log.Fatalln(err) //-> Println(err)호출 뒤 os.Exit(1)불러 프로그램 종료
-	}
-	fmt.Println(account.Balance(), account.Owner())
+	dictionary := mydict.Dictionary{}
+	dictionary["hello"] = "hello"
+	fmt.Println(dictionary)
 }
