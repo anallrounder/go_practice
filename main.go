51b902efadaf3c2773fb5f976a736ff3487bf628
DESKTOP-2BJM56G\Hyeseon <artsmenii@gmail.com>
2021-08-16 21:13:51 +0900

    goroutine_06

--- main.go
+++ main.go
@@ -7,16 +7,18 @@ import (
 
 func main() {
 	c := make(chan string)
-	people := [2]string{"sun", "ogu"}
+	people := [5]string{"sun", "ogu", "sunny", "jm", "hs"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	fmt.Println("Wating for messages")
-	fmt.Println("Received this message: ", <-c)
-	fmt.Println("Received this message: ", <-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
 }
 
 func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 10)
-	c <- person + "is sexy"
+	c <- person + " is sexy"
 }
+
+//이 코드가 다섯개의 리시버를 만든다
