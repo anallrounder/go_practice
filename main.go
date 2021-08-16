165f8e8d7ca1df15c364bea9b51bedaa669e62ee
DESKTOP-2BJM56G\Hyeseon <artsmenii@gmail.com>
2021-08-17 00:43:33 +0900

    urlckecker+goroutine

--- main.go
+++ main.go
@@ -2,24 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
+type result struct {
+	url    string
+	status string
+}
+
 func main() {
-	c := make(chan string)
-	people := [5]string{"sun", "ogu", "sunny", "jm", "hs"}
-	for _, person := range people {
-		go isSexy(person, c)
+	c := make(chan result)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	for i := 0; i < len(people); i++ {
-		fmt.Println("Waiting for ", i)
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
 		fmt.Println(<-c)
 	}
 }
 
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 10)
-	c <- person + " is sexy"
+func hitURL(url string, c chan<- result) {
+	//fmt.Println("Checking  ", url)
+	resp, err := http.Get(url)
+	status := "OK"                            //default value
+	if err != nil || resp.StatusCode >= 400 { //에러 있거나 400이상(에러)
+		status = "FAILED"
+		//c <- result{url: url, status: "FAILED"}
+	}
+	c <- result{url: url, status: status}
+	//c <- result{url: url, status: "OK"}
 }
-
-//이 코드가 다섯개의 리시버를 만든다
