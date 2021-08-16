f01e271bcc8420c6c9c9f1c0f3200baf60d367a7
DESKTOP-2BJM56G\Hyeseon <artsmenii@gmail.com>
2021-08-16 19:17:09 +0900

    goroutine_04

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	//fmt.Println(result)
 	fmt.Println(<-c)
 	fmt.Println(<-c) //두개 받으려면 하나더 추가
-	fmt.Println(<-c)
+	//fmt.Println(<-c) //fatal error: all goroutines are asleep - deadlock!
 }
 
 func isSexy(person string, c chan bool) {
